d81e3caab511b5c746ec529e36a10b0e6c5c51e5
Saadat123456 <saadatlucky313@gmail.com>
2022-03-15 21:33:49 +0500

    Create changeResult Function and Perform Calculations

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,15 +1,58 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 import CalculatorButtons from './CalculatorButtons';
 import Result from './result';
 
 class Calculator extends React.Component {
-  header = () => <div>Good</div>;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      calculation: {
+        total: 0,
+        next: null,
+        operation: null,
+      },
+    };
+  }
+
+  changeResult = (buttonName) => {
+    this.setState((prevState) => {
+      if (buttonName === '=') {
+        if (prevState.calculation.next === null || prevState.calculation.total === null) {
+          return {};
+        }
+      }
+      const newCalculation = calculate(prevState.calculation, buttonName);
+      return { calculation: newCalculation };
+    });
+  }
 
   render() {
+    const { calculation } = this.state;
+
+    let result = '';
+
+    if (calculation.total) {
+      result += `${calculation.total}`;
+
+      if (calculation.operation) {
+        result += ` ${calculation.operation}`;
+
+        if (calculation.next) {
+          result += ` ${calculation.next}`;
+        }
+      }
+    } else if (calculation.next) {
+      result += ` ${calculation.next}`;
+    } else {
+      result = '0';
+    }
+
     return (
       <div>
-        <Result result={0} className="result" />
-        <CalculatorButtons />
+        <Result result={result} className="result" />
+        <CalculatorButtons onClick={this.changeResult} />
       </div>
     );
   }
