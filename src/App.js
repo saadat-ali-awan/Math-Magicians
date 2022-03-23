de06a9641a3eb8f1d726512522e6eb41f7c33b4b
Saadat123456 <saadatlucky313@gmail.com>
2022-03-24 02:41:00 +0500

    Add Header and Navigation Links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import './App.css';
-import Calculator from './components/Calculator';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from 'react-router-dom';
+import Home from './pages/home';
+import Calculator from './pages/calculator';
+import Quote from './pages/quote';
 
 class App extends React.PureComponent {
   render() {
     return (
-      <main className="App">
-        <Calculator />
-      </main>
+      <Router>
+        <header>
+          <h1>Math Magicians</h1>
+          <ul>
+            <NavLink
+              to="/"
+              className={({ isActive }) => `nav-link ${(isActive ? ' selected' : '')}`}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/calculator"
+              className={({ isActive }) => `nav-link ${(isActive ? ' selected' : '')}`}
+            >
+              Calculator
+            </NavLink>
+            <NavLink
+              to="/quote"
+              className={({ isActive }) => `nav-link ${(isActive ? ' selected' : '')}`}
+            >
+              Quote
+            </NavLink>
+          </ul>
+        </header>
+        <Routes>
+          <Route path="/quote" element={<Quote />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     );
   }
 }
