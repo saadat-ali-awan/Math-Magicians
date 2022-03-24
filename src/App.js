8fbe10132428fc4b7ae13abe289bfb26b9bfb6ff
Saadat123456 <saadatlucky313@gmail.com>
2022-03-24 23:16:07 +0500

    Create NavMenu Component

--- src/App.js
+++ src/App.js
@@ -4,42 +4,21 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  NavLink,
 } from 'react-router-dom';
 import Home from './pages/home';
-import Calculator from './pages/calculator';
+import CalculatorPage from './pages/calculator';
 import Quote from './pages/quote';
+import NavMenu from './components/NavMenu';
 
 class App extends React.PureComponent {
   render() {
     return (
       <Router>
-        <header>
-          <h1>Math Magicians</h1>
-          <ul>
-            <NavLink
-              to="/"
-              className={({ isActive }) => `nav-link ${(isActive ? ' selected' : '')}`}
-            >
-              Home
-            </NavLink>
-            <NavLink
-              to="/calculator"
-              className={({ isActive }) => `nav-link ${(isActive ? ' selected' : '')}`}
-            >
-              Calculator
-            </NavLink>
-            <NavLink
-              to="/quote"
-              className={({ isActive }) => `nav-link ${(isActive ? ' selected' : '')}`}
-            >
-              Quote
-            </NavLink>
-          </ul>
-        </header>
+        <div className="backdrop" />
+        <NavMenu />
         <Routes>
           <Route path="/quote" element={<Quote />} />
-          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/calculator" element={<CalculatorPage />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
